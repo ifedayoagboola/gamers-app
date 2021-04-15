94151d85c54bfe07e0bb8d5c81fcbaa813b0d56a
ifedayo <ifedayoagboola19@gmail.com>
2021-04-15 23:35:12 +0100

    Game maker icons

--- src/components/GameDetail.js
+++ src/components/GameDetail.js
@@ -8,6 +8,14 @@ import { useHistory } from "react-router-dom";
 
 import { smallImage } from "../util";
 
+//IMAGES
+import playstation from "../images/playstation.svg";
+import steam from "../images/steam.svg";
+import xbox from "../images/xbox.svg";
+import nintendo from "../images/nintendo.svg";
+import apple from "../images/apple.svg";
+import gamepad from "../images/gamepad.svg";
+
 const GameDetail = ({ pathId }) => {
   const history = useHistory();
   //Exit Detail
@@ -16,6 +24,23 @@ const GameDetail = ({ pathId }) => {
     if (element.classList.contains("shadow")) {
       document.body.style.overflow = "auto";
       history.push("/");
+    }
+  };
+  //GET PLATFORM IMAGES
+  const getPlatform = (platform) => {
+    switch (platform) {
+      case "PlayStation 4":
+        return playstation;
+      case "Xbox One":
+        return xbox;
+      case "PC":
+        return steam;
+      case "Nintendo Switch":
+        return nintendo;
+      case "iOS":
+        return apple;
+      default:
+        return gamepad;
     }
   };
   //Data
@@ -34,7 +59,11 @@ const GameDetail = ({ pathId }) => {
                 <h3>Platforms</h3>
                 <Platforms>
                   {game.platforms.map((data) => (
-                    <h3 key={data.platform.id}>{data.platform.name}</h3>
+                    <img
+                      alt={data.platform.name}
+                      key={data.platform.id}
+                      src={getPlatform(data.platform.name)}
+                    ></img>
                   ))}
                 </Platforms>
               </Info>
@@ -73,6 +102,7 @@ const CardShadow = styled(motion.div)`
   position: fixed;
   top: 0;
   left: 0;
+  z-index: 5;
   &::-webkit-scrollbar {
     width: 0.5rem;
   }
@@ -92,6 +122,7 @@ const Detail = styled(motion.div)`
   position: absolute;
   left: 10%;
   color: black;
+  z-index: 10;
   img {
     width: 100%;
   }
